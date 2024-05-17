741f137afe70ab78d25e8dab49451b021a778893
Mnafyndiaye <111544993+Mnafyndiaye@users.noreply.github.com>
2024-05-17 20:50:40 +0000

    je sais pas

--- PasswordCracker/Main.java
+++ PasswordCracker/Main.java
@@ -1,5 +1,13 @@
 package PasswordCracker;
 
 public class Main {
-    
+    public static void main(String[] args) throws Exception{
+
+
+        System.out.println("*******************************");
+        System.out.println("Bienvenue dans notre plateforme");
+        System.out.println("*******************************");
+        System.out.println("");
+
+    }
 }
